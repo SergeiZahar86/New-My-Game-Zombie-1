4c95554a1745a7725bc947d39eabe3ebea90d431
Sergei Zakharov <sergei86zahar@mail.ru>
2020-08-28 13:23:10 +0700

    rename script

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -2,14 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class character : MonoBehaviour
+public class CharacterMovement : MonoBehaviour
 {
     public float moveSpeed;
     public Rigidbody2D rb;
     private Vector2 moveDirection;
 
-    // Update is called once per frame
-    void Update()
+    void Update ()
     {
         ProcessInputs ();
     }
@@ -19,13 +18,10 @@ public class character : MonoBehaviour
     }
     void ProcessInputs ()
     {
-        float moveX = Input.GetAxisRaw ("Horizontal");
-        float moveY = Input.GetAxisRaw ("Vertical");
-        moveDirection = new Vector2 (moveX, moveY).normalized; // TODO come back to this
+        moveDirection = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical")).normalized;
     }
     void Move ()
     {
         rb.velocity = new Vector2 (moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
     }
 }
-
