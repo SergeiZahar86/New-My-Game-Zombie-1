b4d314329a0137396c6b55a9f3596e5df980c68b
Sergei Zakharov <sergei86zahar@mail.ru>
2020-08-29 10:17:40 +0700

    начал заново. только скрипт игрока и головы

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+        public float speed; // скорость пули
+    public float lifetime; // время жизни пули
+    public float distance; // расстояние полёта пули
+    public int damage; // урон
+    public LayerMask whatIsSolid; // задаёт слои для пробития
+
+    private void Update ()
+    {
+        // найти объект для пробития
+        RaycastHit2D hitInfo = Physics2D.Raycast (transform.position, transform.up, distance, whatIsSolid);
+        // если пуля столкнулась с каким-нибудь коллайдером и у него тег "Эними" (наш враг), 
+        // то мы наносим этому врагу урон и уничтожаем пулю
+        if(hitInfo.collider != null)
+        {
+            if (hitInfo.collider.CompareTag ("Enemy"))
+            {
+                hitInfo.collider.GetComponent<Enemy> ().TakeDamage (damage);
+            }
+            Destroy (gameObject);
+            
+        }
+        // движение патрона
+        transform.Translate (Vector2.up * speed * Time.deltaTime);
+    }
+
+}
