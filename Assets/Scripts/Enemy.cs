87d8c7bca687b9642be77d3db2f6955128d172a0
Sergei Zakharov <sergei86zahar@mail.ru>
2020-08-29 17:14:33 +0700

    скрипт врага enemy  (идёт влево)

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int health;
+    public float speed;
+    private void Update ()
+    {
+        if(health <= 0)
+        {
+            Destroy (gameObject); // уничтожаем врага если не осталось здоровья
+        }
+        transform.Translate (Vector2.left * speed * Time.deltaTime);
+    }
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+    }
+}
