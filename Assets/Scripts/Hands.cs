bc2f21c1c4f428908cac18dab5514666ddeb17e5
Sergei Zakharov <sergei86zahar@mail.ru>
2020-08-29 16:54:33 +0700

    добаваил скрипт рук

--- Assets/Scripts/Hands.cs
+++ Assets/Scripts/Hands.cs
@@ -4,23 +4,20 @@ using UnityEngine;
 
 public class Hands : MonoBehaviour
 {
-   public float offset; // величина для корректировки вращения рук
+    public float offset; // величина для корректировки вращения рук
     public GameObject bullet; // создаём пулю
     public Transform shotPoint; // место откуда будет лететь пуля
-    private float timeBtwShots; 
+    private float timeBtwShots;
     public float startTimeBtwShots; // время между выстрелами
 
     private void Update ()
     {
         //рассчитать положение курсора и повернуть в это направление оружие
-
         Vector3 difference = Camera.main.ScreenToWorldPoint (Input.mousePosition) - transform.position;
         float rotZ = Mathf.Atan2 (difference.y, difference.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler (0f, 0f, rotZ + offset);
-
         // произвести выстрел и перезарядиться
-
-        if (timeBtwShots <=0) 
+        if (timeBtwShots <= 0)
         {
             if (Input.GetMouseButton (0))
             {
